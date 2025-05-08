d290b51ea0c6354a21fcac801576f3a7338ee6ba
MICHAEL DE LIMA FLORENCIO <michael.lflorencio@senacsp.edu.br>
2025-05-08 11:45:52 -0300

    Incluindo os Forms

--- ComercialTDSDesk/FrmPrincipal.Designer.cs
+++ ComercialTDSDesk/FrmPrincipal.Designer.cs
@@ -1,14 +1,14 @@
 ﻿namespace ComercialTDSDesk
 {
-    partial class Form1
+    partial class FrmPrincipal
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,21 +23,23 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             SuspendLayout();
             // 
-            // Form1
+            // FrmPrincipal
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Name = "Form1";
-            Text = "Form1";
-            Load += Form1_Load;
+            IsMdiContainer = true;
+            Name = "FrmPrincipal";
+            Text = "Sistema Comercial TDS T01";
+            WindowState = FormWindowState.Maximized;
+            Load += FrmPrincipal_Load;
             ResumeLayout(false);
         }
 
