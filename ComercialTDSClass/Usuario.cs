0c04499aaa7e8abaacad517808c78756154fad2e
MICHAEL DE LIMA FLORENCIO <michael.lflorencio@senacsp.edu.br>
2025-05-12 09:04:52 -0300

    implementação das classes banco e nivel, e correção

--- ComercialTDSClass/Usuario.cs
+++ ComercialTDSClass/Usuario.cs
@@ -1,12 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ComercialTDSClass
 {
-    class Usuario
+    public class Usuario
     {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public string? Senha { get; set; }
+        public Nivel? Nivel { get; set; }
+        public bool Ativo { get; set; }
+        public Usuario() { }// construtor vazio
+        public Usuario(int id, string? nome, string? email, string? senha, Nivel? nivel, bool ativo)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+            Nivel = nivel;
+            Ativo = ativo;
+        }
+        public Usuario(string? nome, string? email, string? senha, Nivel? nivel, bool ativo)
+        {
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+            Nivel = nivel;
+            Ativo = ativo;
+        }
+        public Usuario(string? nome, string? email, string? senha, Nivel? nivel)
+        {
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+            Nivel = nivel;
+        }
+        public Usuario(int id, string? senha)
+        {
+            Id = id;
+            Senha = senha;
+        }
+        public Usuario(int id, string? nome, string? senha, Nivel? nivel, bool ativo)
+        {
+            Id = id;
+            Nome = nome;
+            Senha = senha;
+            Nivel = nivel;
+            Ativo = ativo;
+        }
+        public void Inserir()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_usuario_insert";
+            cmd.Parameters.AddWithValue("spnome", Nome);
+            cmd.Parameters.AddWithValue("spemail", Email);
+            cmd.Parameters.AddWithValue("spsenha", Senha);
+            cmd.Parameters.AddWithValue("spnivel", Nivel.Id);
+
+        }
+        public bool Atualizar()
+        {
+            return true;
+        }
+        public static Usuario ObterPorId(int id)
+        {
+            Usuario usuario = new();
+
+            return usuario;
+        }
+        public static List<Usuario> ObterLista()
+        {
+            List<Usuario> usuarios = new();
+
+            return usuarios;
+        }
+        public static Usuario EfatuarLogin(string email, string senha)
+        {
+            Usuario usuario = new();
+
+            return usuario;
+        }
+        public static bool AlterarSenha(string email, string senha)
+        {
+            return true;
+        }
+
+
     }
 }
+
+
