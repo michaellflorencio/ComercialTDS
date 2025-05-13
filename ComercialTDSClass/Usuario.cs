66565f7203e9e06df5dea56d1e932e3cfbcf008d
MICHAEL DE LIMA FLORENCIO <michael.lflorencio@senacsp.edu.br>
2025-05-13 09:18:48 -0300

    classes e metodos

--- ComercialTDSClass/Usuario.cs
+++ ComercialTDSClass/Usuario.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace ComercialTDSClass
 {
@@ -15,7 +15,10 @@ namespace ComercialTDSClass
         public string? Senha { get; set; }
         public Nivel? Nivel { get; set; }
         public bool Ativo { get; set; }
-        public Usuario() { }// construtor vazio
+        public Usuario()
+        {
+            Nivel = new(); // composição uml  
+        }// construtor vazio
         public Usuario(int id, string? nome, string? email, string? senha, Nivel? nivel, bool ativo)
         {
             Id = id;
@@ -39,6 +42,7 @@ namespace ComercialTDSClass
             Email = email;
             Senha = senha;
             Nivel = nivel;
+
         }
         public Usuario(int id, string? senha)
         {
@@ -64,28 +68,28 @@ namespace ComercialTDSClass
             cmd.Parameters.AddWithValue("spnivel", Nivel.Id);
             Id = Convert.ToInt32(cmd.ExecuteScalar());
             cmd.Connection.Close();
-
         }
         public bool Atualizar()
         {
             var cmd = Banco.Abrir();
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.CommandText = "sp_usuario_altera";
-            cmd.Parameters.AddWithValue("spid",Id);
+            cmd.Parameters.AddWithValue("spid", Id);
             cmd.Parameters.AddWithValue("spnome", Nome);
             cmd.Parameters.AddWithValue("spsenha", Senha);
             cmd.Parameters.AddWithValue("spnivel", Nivel.Id);
             // usando if ternário, sem fechar conexão
             return cmd.ExecuteNonQuery() > 0 ? true : false;
 
-            //// usando if/else e fechando a conexão 
+            // usando if/else e fechando a conexão
             //if (cmd.ExecuteNonQuery() > 0)
-            //{ 
+            //{
             //    cmd.Connection.Close();
-            //return true;
+            //    return true;
             //}
             //else
             //    return false;
+
         }
         public static Usuario ObterPorId(int id)
         {
@@ -112,7 +116,7 @@ namespace ComercialTDSClass
         {
             List<Usuario> usuarios = new();
             var cmd = Banco.Abrir();
-            cmd.CommandText = $"select * from usuarios where order by nome";
+            cmd.CommandText = $"select * from usuarios order by nome";
             var dr = cmd.ExecuteReader();
             while (dr.Read())
             {
@@ -123,8 +127,8 @@ namespace ComercialTDSClass
                             dr.GetString(3),
                             Nivel.ObterPorId(dr.GetInt32(4)),
                             dr.GetBoolean(5)
-                            )
-                        );
+                        )
+                    );
             }
             dr.Close();
             cmd.Connection.Close();
@@ -134,7 +138,7 @@ namespace ComercialTDSClass
         {
             Usuario usuario = new();
             var cmd = Banco.Abrir();
-            cmd.CommandText = $"select * from usuarios where email = {email} and senha  = {senha}";
+            cmd.CommandText = $"select * from usuarios where email = '{email}' and senha = md5('{senha}') ";
             var dr = cmd.ExecuteReader();
             if (dr.Read())
             {
@@ -149,19 +153,13 @@ namespace ComercialTDSClass
             }
             dr.Close();
             cmd.Connection.Close();
-
             return usuario;
         }
         public static bool AlterarSenha(string email, string senha)
         {
             var cmd = Banco.Abrir();
-            cmd.CommandText = $"update usuarios set senha = md5({senha}) where email = {email}";
+            cmd.CommandText = $"update usuarios set senha = md5('{senha}') where email = '{email}'";
             return cmd.ExecuteNonQuery() > 0 ? true : false;
-            
         }
-
-
     }
 }
-
-
